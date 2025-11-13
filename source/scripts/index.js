ff84e5fd649299fe97ad9598c379c5a250df0eb4
Никитин Сергей <Nikitin99S@gmail.com>
2025-11-13 15:27:17 +0700

    Добавляет скрпит на открытие/закрытие мобильного меню + базовую сетку в мобильной версии для footer

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,22 @@
 /* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  const navElement = document.querySelector('.main-nav');
+  const toggleButton = document.querySelector('.main-header__toggle');
+
+  if (navElement && toggleButton) {
+
+    toggleButton.addEventListener('click', () => {
+
+      navElement.classList.toggle('main-nav--close');
+      navElement.classList.toggle('main-nav--open');
+
+      toggleButton.classList.toggle('toggle--active');
+
+      const isMenuOpen = navElement.classList.contains('main-nav--open');
+      toggleButton.setAttribute('aria-label', isMenuOpen ? 'Закрыть меню' : 'Открыть меню');
+
+    });
+
+  }
+});
